0a1cb65724084d045226c160b94760aacecb9ca3
brendogk <brendo2742@gmail.com>
2025-01-11 18:51:37 +0000

    Arquivos IaC v.1

--- criar_user.sh
+++ criar_user.sh
@@ -3,16 +3,16 @@
 
 echo "Criando usuários do sistema...."
 
-useradd guest10 -c "Usuário convidado" -s /bin/bash -m -p $(openssl passwd -crypt Senha123)
-passwd guest10 -e
+useradd convidado10 -c "Usuário convidado10" -s /bin/bash -m -p $(openssl passwd -6 Senha123)
+passwd convidado10 -e
 
-useradd guest11 -c "Usuário convidado" -s /bin/bash -m -p $(openssl passwd -crypt Senha123)
-passwd guest11 -e
+useradd convidado11 -c "Usuário convidado11" -s /bin/bash -m -p $(openssl passwd -6 Senha123)
+passwd convidadot11 -e
 
-useradd guest12 -c "Usuário convidado" -s /bin/bash -m -p $(openssl passwd -crypt Senha123)
-passwd guest12 -e
+useradd convidado12 -c "Usuário convidado12" -s /bin/bash -m -p $(openssl passwd -6 Senha123)
+passwd covidado12 -e
 
-useradd guest13 -c "Usuário convidado" -s /bin/bash -m -p $(openssl passwd -crypt Senha123)
-passwd guest13 -e
+useradd convidado13 -c "Usuário convidado13" -s /bin/bash -m -p $(openssl passwd -6 Senha123)
+passwd convidado13 -e
 
 echo "Finalizado!!"
